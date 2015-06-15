bb87b4bf92b593faa8e316f100764da29b311c62
Michael Pham <mpham@nativecode.com>
2015-06-15 03:20:20 -0500

    Adding view models

---
+++ src/Demo/Demo/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo.ViewModels
+{
+    using System.Windows.Input;
+
+    using PropertyChanged;
+
+    using Xamarin.Forms;
+
+    public class MainViewModel : ViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+            this.ShowSnackBar = new Command(HandleShowSnackBar);
+            this.Title = "Main";
+        }
+
+        /// <summary>
+        /// Gets the show snack bar command.
+        /// </summary>
+        [DoNotNotify]
+        public ICommand ShowSnackBar { get; private set; }
+
+        private static void HandleShowSnackBar()
+        {
+        }
+    }
+}
