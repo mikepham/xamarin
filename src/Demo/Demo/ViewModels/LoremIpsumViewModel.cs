d7638833a656e0c962c1eda7a2f59f26c51ecc50
Michael Pham <mpham@nativecode.com>
2015-06-17 13:58:44 -0500

    - Added lorem ipsum page.

---
+++ src/Demo/Demo/ViewModels/LoremIpsumViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo.ViewModels
+{
+    using System.IO;
+    using System.Reflection;
+
+    public class LoremIpsumViewModel : ViewModel
+    {
+        private const string LoremIpsumResourceKey = "Demo.Resources.loremipsum.txt";
+
+        public LoremIpsumViewModel()
+        {
+            var stream = this.GetType().GetTypeInfo().Assembly.GetManifestResourceStream(LoremIpsumResourceKey);
+
+            using (var reader = new StreamReader(stream))
+            {
+                this.LoremIpsumText = reader.ReadToEnd();
+            }
+        }
+
+        public string LoremIpsumText { get; set; }
+    }
+}
