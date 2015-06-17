ae3634a505b2717477de84575a74996e97f8c379
Michael Pham <mpham@nativecode.com>
2015-06-16 21:26:24 -0500

    Removed the AppCompatMasterDetailRenderer again.

--- src/NativeCode.Mobile.AppCompat.Renderers/Renderers/AppCompatMasterDetailRenderer.cs
+++
@@ -1,8 +0,0 @@
-namespace NativeCode.Mobile.AppCompat.Renderers.Renderers
-{
-    using Xamarin.Forms.Platform.Android;
-
-    public class AppCompatMasterDetailRenderer : MasterDetailRenderer
-    {
-    }
-}
